324d4d4fa412c96eb75f94ae694d1d5f72e3c8e7
Strahinja Djukic <strahinjadjukic95@gmail.com>
2025-09-02 15:00:12 +0200

    Added navigation and background for app

--- ValorQuest/app/src/main/java/com/example/valorquest/ui/MainActivity.java
+++ ValorQuest/app/src/main/java/com/example/valorquest/ui/MainActivity.java
@@ -39,12 +39,10 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+        binding.fab.setOnClickListener(view -> {
+            if (navController.getCurrentDestination() != null &&
+                    navController.getCurrentDestination().getId() != R.id.mainMenuFragment) {
+                navController.navigate(R.id.mainMenuFragment);
             }
         });
     }

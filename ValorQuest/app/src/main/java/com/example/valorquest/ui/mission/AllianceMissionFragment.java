d611603b367f5addf1e41ea7230860e905acd1bc
Strahinja Djukic <strahinjadjukic95@gmail.com>
2025-10-03 02:19:39 +0200

    User contributions added

--- ValorQuest/app/src/main/java/com/example/valorquest/ui/mission/AllianceMissionFragment.java
+++ ValorQuest/app/src/main/java/com/example/valorquest/ui/mission/AllianceMissionFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -11,22 +12,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.valorquest.R;
 import com.example.valorquest.model.dto.MissionSummaryDto;
+import com.example.valorquest.model.dto.UserContributionDto;
 import com.example.valorquest.viewmodel.AllianceMissionViewModel;
 import com.google.android.material.button.MaterialButton;
+
+import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class AllianceMissionFragment extends Fragment {
     private AllianceMissionViewModel viewModel;
-    private TextView missionTitle;
+    private TextView missionTitle, tvBossHpText;
     private MaterialButton btnMission;
     private MissionSummaryDto missionSummary;
+
+    private ConstraintLayout missionContainer;
+    private TextView tvNoMission;
+
+    private ProgressBar progressBossHp;
 
     @Nullable
     @Override
@@ -35,6 +46,11 @@ public class AllianceMissionFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_alliance_mission, container, false);
         missionTitle = view.findViewById(R.id.missionTitle);
         btnMission = view.findViewById(R.id.btnMission);
+        progressBossHp = view.findViewById(R.id.progressBossHp);
+        tvBossHpText = view.findViewById(R.id.tvBossHpText);
+        missionContainer = view.findViewById(R.id.missionContainer);
+        tvNoMission = view.findViewById(R.id.tvNoMission);
+
         return view;
     }
 
@@ -59,9 +75,23 @@ public class AllianceMissionFragment extends Fragment {
         viewModel.getActiveMissionSummary().observe(getViewLifecycleOwner(), summary -> {
             missionSummary = summary;
 
-            if (summary == null) {
-            } else {
-                // TODO: Update other mission details in UI if needed
+            if (summary != null) {
+                missionContainer.setVisibility(View.VISIBLE);
+                tvNoMission.setVisibility(View.GONE);
+
+                int originalHp = missionSummary.originalBossHp;
+                int currentHp = missionSummary.currentBossHp;
+                int progress = (int) ((currentHp / (float) originalHp) * 100);
+                progressBossHp.setMax(100);
+                progressBossHp.setProgress(progress);
+
+                tvBossHpText.setText(currentHp + " / " + originalHp);
+
+                displayUserContributions();
+            }
+            else{
+                missionContainer.setVisibility(View.GONE);
+                tvNoMission.setVisibility(View.VISIBLE);
             }
 
             handleMissionButton(summary);
@@ -84,6 +114,32 @@ public class AllianceMissionFragment extends Fragment {
             });
         } else {
             btnMission.setVisibility(View.GONE);
+        }
+    }
+    private void displayUserContributions() {
+        LinearLayout container = requireView().findViewById(R.id.missionDetailsContainer);
+        container.removeAllViews();
+
+        LayoutInflater inflater = LayoutInflater.from(requireContext());
+
+        for (UserContributionDto contribution : missionSummary.contributions) {
+            View itemView = inflater.inflate(R.layout.item_user_contribution, container, false);
+
+            TextView tvUsername = itemView.findViewById(R.id.tvUsername);
+            ProgressBar pbContribution = itemView.findViewById(R.id.pbUserContribution);
+            TextView tvValue = itemView.findViewById(R.id.tvContributionValue);
+
+            tvUsername.setText(contribution.username);
+
+            int maxHp = missionSummary.originalBossHp;
+            int current = contribution.totalHpDealt;
+
+            pbContribution.setMax(maxHp);
+            pbContribution.setProgress(current);
+
+            tvValue.setText(current + " / " + maxHp);
+
+            container.addView(itemView);
         }
     }
 }
